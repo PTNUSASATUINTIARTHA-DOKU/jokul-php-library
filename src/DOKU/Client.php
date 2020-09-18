a06508c35d04ef0ad2bcc550ee6642d2987205eb
Darien <dokudevex@gmail.com>
2020-09-18 16:39:35 +0700

    Update Client.php

--- src/DOKU/Client.php
+++ src/DOKU/Client.php
@@ -37,8 +37,50 @@ class Client
 
     public function generateMandiriVa($params)
     {
-        $va = new VirtualAccount;
-        $va->generateMandiriVa($params);
+        $this->config = $this->client->getConfig();
+        $words = $this->generateWords($params);
+
+        $data = array(
+            "client" => array(
+                "id" => $this->config['client_id']
+            ),
+            "order" => array(
+                "invoice_number" => $params['invoiceNumber'],
+                "amount" => $params['amount']
+            ),
+            "virtual_account_info" => array(
+                "expired_time" => 60,
+                "reusable_status" => 'false',
+            ),
+            "customer" => array(
+                "name" => trim($params['customerName']),
+                "email" => $params['customerEmail']
+            ),
+            "security" => array(
+                "check_sum" => $words
+            )
+        );
+
+        if ($this->config['environment'] == Client::SANDBOX) {
+            $url = 'http://dev.dokupay.com/mandiri-virtual-account/v1/payment-code';
+        } else {
+            $url = 'http: //app-sit.doku.com/mandiri-virtual-account/v1/payment-code';
+        }
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $responseJson = curl_exec($ch);
+
+        curl_close($ch);
+
+        if (is_string($responseJson)) {
+            // echo json_decode($responseJson, true);
+            print_r($responseJson);
+        } else {
+            print_r($responseJson);
+        }
         
     }
 
