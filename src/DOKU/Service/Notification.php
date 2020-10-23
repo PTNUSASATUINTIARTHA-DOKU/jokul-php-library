eadc5480d6f193616860fe36126301bc57a0bc5c
Darien <dokudevex@gmail.com>
2020-10-23 18:18:06 +0700

    Update Notification.php

--- src/DOKU/Service/Notification.php
+++ src/DOKU/Service/Notification.php
@@ -25,6 +25,25 @@ class Notification
             $raw_notification = json_decode(file_get_contents('php://input'), true);
             $this->response = $raw_notification;
         }
+        $this->returnResponse($this->response);
         return $this->response;
+    }
+
+    function returnResponse($json_data_input)
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+
+        $json_data_output = array(
+            "order" => array(
+                "invoice_number" => $json_data_input['order']['invoice_number'],
+                "amount" => $json_data_input['order']['amount']
+            ),
+            "virtual_account_info" => array(
+                "virtual_account_number" => $json_data_input['virtual_account_info']['virtual_account_number']
+            )
+        );
+
+        echo json_encode($json_data_output);
     }
 }
