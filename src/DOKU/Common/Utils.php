d52e461ce9727b19f9d3d36007f600d6fcd4dc9b
Arief R Ramadhan <arief.rizky@doku.com>
2021-05-08 05:50:48 +0700

    Update generate signature to support GET method, Update Request VA Header Request

--- src/DOKU/Common/Utils.php
+++ src/DOKU/Common/Utils.php
@@ -7,12 +7,20 @@ class Utils
     public static function generateSignature($headers, $targetPath, $body, $secret)
     {
         $digest = base64_encode(hash('sha256', $body, true));
-        $rawSignature = "Client-Id:" . $headers['Client-Id'] . "\n"
+        
+        if (isset($digest)) {
+            $rawSignature = "Client-Id:" . $headers['Client-Id'] . "\n"
             . "Request-Id:" . $headers['Request-Id'] . "\n"
             . "Request-Timestamp:" . $headers['Request-Timestamp'] . "\n"
             . "Request-Target:" . $targetPath . "\n"
             . "Digest:" . $digest;
-
+        } else {
+            $rawSignature = "Client-Id:" . $headers['Client-Id'] . "\n"
+            . "Request-Id:" . $headers['Request-Id'] . "\n"
+            . "Request-Timestamp:" . $headers['Request-Timestamp'] . "\n"
+            . "Request-Target:" . $targetPath;
+        }
+        
         $signature = base64_encode(hash_hmac('sha256', $rawSignature, $secret, true));
         return 'HMACSHA256=' . $signature;
     }
